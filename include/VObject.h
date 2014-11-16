4e3c75c5e7ac9a99df3c9ee801147f149c7249a5
LLafodog <silvert.ervan@gmail.com>
2014-11-16 17:11:50 +0100

    Ajout d'une animation suivant l'orientation de l'objet.
    Ajout donc d'une classe VObject (pour Visual of Object) qui garde un oeil sur l'objet et actualise son animation en fonction des changements.
    De fait, Graphics::initObjects() utilise maintenant VObject.

---
+++ include/VObject.h
@@ -0,0 +1,27 @@
+#ifndef VOBJECT_H
+#define VOBJECT_H
+
+#include "Tile.h"
+
+class Object;
+class Animation;
+
+class VObject : public Tile
+{
+    public:
+
+        VObject(Object* o);
+
+        void update();
+        void changeOrientation(char c);
+
+        virtual ~VObject();
+
+    protected:
+        std::vector<Animation*> m_animations;
+        Object* m_object;
+        char m_orientation;
+
+};
+
+#endif // VOBJECT_H
---
+++ src/VObject.cpp
@@ -0,0 +1,41 @@
+#include "VObject.h"
+
+#include "Object.h"
+#include "AnimationEngine.h""
+
+using namespace std;
+
+VObject::VObject(Object* o): Tile(o->getId(),o->getPositionX(), o->getPositionY(),o->isVisible(),o->getSize().x,o->getSize().y)
+{
+    m_object=o;
+    m_orientation='x';
+    m_animations=AnimationEngine::getAllOf("dragon");
+    update();
+
+}
+
+void VObject::update()
+{
+    char currentOrientation=m_object->getOrientation();
+    if(currentOrientation!=m_orientation)changeOrientation(currentOrientation);
+    setPosition(m_object->getPosition());
+    Tile::update();
+
+}
+
+void VObject::changeOrientation(char c)
+{
+    switch(c)
+    {
+        case 'n': m_animation=m_animations[0];break;
+        case 'e': m_animation=m_animations[1];break;
+        case 's': m_animation=m_animations[2];break;
+        case 'w': m_animation=m_animations[3];break;
+        default: m_animation=AnimationEngine::get("error");
+    }
+}
+
+VObject::~VObject()
+{
+    //dtor
+}
